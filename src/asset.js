756c7f7ef1124041e8fc48a073ad8b47ecd559c8
Judicaël Anouma <janouma@gmail.com>
2018-05-08 18:01:40 +0200

    cleaned debug statments

--- src/asset.js
+++ src/asset.js
@@ -1,19 +1,7 @@
 'use strict'
 
-// const {readFile} = require('fs')
 const {Asset} = require('parcel-bundler')
-
-/* function read (file) {
-  return new Promise((resolve, reject) => {
-    readFile(file, (error, content) => {
-      if (!error) {
-        resolve(content)
-      } else {
-        reject(error)
-      }
-    })
-  })
-} */
+const log = require('loglevel')
 
 module.exports = class TextAsset extends Asset {
   constructor (name, pkg, options) {
@@ -26,39 +14,12 @@ module.exports = class TextAsset extends Asset {
       .replace(/\r|\n/g, '')
       .replace(/'/g, "\\'")
 
-    // DEBUG
-    console.log({
-      loaded: this.name // ,
-      // content
-    })
-    // END DEBUG
+    log.debug({ loaded: this.name })
+    log.trace({content})
 
     return [{
       type: 'js',
       value: `module.exports = '${content}'`
     }]
-
-    /* // require coffeescript, installed locally in the app
-    let coffee = await localRequire('coffeescript', this.name);
-
-    // Transpile Module using CoffeeScript and parse result as ast format through babylon
-    let transpiled = coffee.compile(this.contents, {
-      sourceMap: this.options.sourceMaps
-    });
-
-    let sourceMap;
-    if (transpiled.sourceMap) {
-      sourceMap = transpiled.sourceMap.generate();
-      sourceMap.sources = [this.relativeName];
-      sourceMap.sourcesContent = [this.contents];
-    }
-
-    return [
-      {
-        type: 'js',
-        value: this.options.sourceMaps ? transpiled.js : transpiled,
-        sourceMap
-      }
-    ]; */
   }
 }
